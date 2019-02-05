90180428b03636efc5ea115d3675ea9ad9e8cad6
Vaibhav Karve <vaibhavskarve@gmail.com>
2019-02-05 13:58:24 -0600

    remove remaining dependence on unittest

--- test_hashable_counter.py
+++ test_hashable_counter.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
-import unittest
 import collections
 from hashable_counter import frozencounter as fc
 
-class TestConstructors(unittest.TestCase):
-    def test_basic_properties(self):
-        assert fc('aab') == fc('aab')
-        assert fc('aab') == fc('aba')
-        assert fc('aab') != fc('ab')
-        assert fc() == {}
-        assert fc() == collections.Counter()
+def test_basic_properties():
+    assert fc('aab') == fc('aab')
+    assert fc('aab') == fc('aba')
+    assert fc('aab') != fc('ab')
+    assert fc() == {}
+    assert fc() == collections.Counter()
 
-    def test_nested_properties(self):
-        assert fc([fc('aab'), fc('aba')]) == fc({fc('aab'): 2})
-        assert fc(fc()) == {}
-        assert fc(fc()) == collections.Counter()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_nested_properties():
+    assert fc([fc('aab'), fc('aba')]) == fc({fc('aab'): 2})
+    assert fc(fc()) == {}
+    assert fc(fc()) == collections.Counter()
