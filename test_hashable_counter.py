4fd106e674c4bccecec3bae7f394be5874b4e512
Vaibhav Karve <vaibhavskarve@gmail.com>
2019-02-05 13:51:33 -0600

    refactor tests for hashable_counter using unittest2pytest

--- test_hashable_counter.py
+++ test_hashable_counter.py
@@ -6,16 +6,16 @@ from hashable_counter import frozencounter as fc
 
 class TestConstructors(unittest.TestCase):
     def test_basic_properties(self):
-        self.assertEqual(fc('aab'), fc('aab'))
-        self.assertEqual(fc('aab'), fc('aba'))
-        self.assertNotEqual(fc('aab'), fc('ab'))
-        self.assertEqual(fc(), {})
-        self.assertEqual(fc(), collections.Counter())
+        assert fc('aab') == fc('aab')
+        assert fc('aab') == fc('aba')
+        assert fc('aab') != fc('ab')
+        assert fc() == {}
+        assert fc() == collections.Counter()
 
     def test_nested_properties(self):
-        self.assertEqual(fc([fc('aab'), fc('aba')]), fc({fc('aab'): 2}))
-        self.assertEqual(fc(fc()), {})
-        self.assertEqual(fc(fc()), collections.Counter())
+        assert fc([fc('aab'), fc('aba')]) == fc({fc('aab'): 2})
+        assert fc(fc()) == {}
+        assert fc(fc()) == collections.Counter()
 
 if __name__ == '__main__':
     unittest.main()
