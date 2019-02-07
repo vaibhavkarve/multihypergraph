2c4c20efb806316407abac2a50bc7f04f1422c43
Vaibhav Karve <vaibhavskarve@gmail.com>
2019-02-07 16:22:07 -0600

    fix LICENSE classifier

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
