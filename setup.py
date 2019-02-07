77cf35a67bad0f7b4c602b511f4e69b6668737d1
Vaibhav Karve <vaibhavskarve@gmail.com>
2019-02-07 15:58:07 -0600

    add package details to setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="multihypergraph",
+    version="0.0.1",
+    author="Vaibhav Karve",
+    author_email="vaibhavskarve@gmail.com",
+    description="A simple python package for graph theory that supports multi-edges, hyper-edges, looped-edges and every other combination of these.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vaibhavkarve/multihypergraph",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",
+    ],
+)
